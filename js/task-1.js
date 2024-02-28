855e35da4858803c53a7a32a8d2eaeb2672e81c6
ValentynaVK <valia2526.vk@gmail.com>
2024-02-28 21:53:50 +0200

    new

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,11 @@
-const list = document.querySelector('#categories');
-const items = [...list.children];
-console.log(`Number of categories: ${items.length}`);
-items.forEach(item => {
-  console.log(`category: ${item.querySelector('h2').textContent}`);
-  console.log(`Elements: ${item.querySelector('ul').children.length}`);
-});
+
+
+
+
+
+
+
+
+
+
+
